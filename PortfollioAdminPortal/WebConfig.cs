a233b9e1fb6fb1f7692401cd4350303c1291bfa4
Leigh966 <leigh966@hotmail.com>
2024-02-01 13:14:40 +0000

    made backend url a constant

---
+++ PortfollioAdminPortal/WebConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfollioAdminPortal
+{
+    public static class WebConfig
+    {
+        public const string BACKEND_URL = "http://127.0.0.1:3000";
+    }
+}
