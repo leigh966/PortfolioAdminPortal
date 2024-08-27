4f361d6c76ad81a58cc05f8279b750217b8eae18
Leigh966 <leigh966@hotmail.com>
2024-08-27 14:20:55 +0100

    added a server entity class for education

---
+++ PortfollioAdminPortal/Education.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfollioAdminPortal
+{
+    public class Education : ServerEntity
+    {
+        public string School { get; set; }
+        public string Course { get; set; }
+        public string StartDate {  get; set; }
+        public string EndDate { get; set; }
+
+        public Education(string id, string school, string course, string startDate, string endDate) : base(id)
+        {
+            School = school;
+            Course = course;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public Education() : base("-1")
+        {
+
+        }
+
+        public override string DisplayText => School + " - " + Course;
+
+        public override string EndpointRoute => WebConfig.BACKEND_URL+"/education/";
+    }
+}
